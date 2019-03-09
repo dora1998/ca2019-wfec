4ceb24ebeb76a2d1c35468a3fcab82fa6bedbc0f
dora1998 <dorayaki.app@gmail.com>
2019-03-09 23:04:10 +0900

    被りがある場合の追加に対応

--- store/index.js
+++ store/index.js
@@ -10,11 +10,25 @@ export const mutations = {
     state.images = newImgs
   },
   addImages(state, newImgs) {
-    if (newImgs.length === 0) {
+    if (newImgs.length === 0 || newImgs.length < IMG_LIMIT) {
       state.isLoadComplete = true
       return
     }
-    state.images.push(...newImgs)
+
+    if (state.images.length === 0) {
+      state.images.push(...newImgs)
+    } else {
+      // 被りがある場合は除く
+      const lastImgId = state.images[state.images.length - 1].id
+      const newImgIds = newImgs.map(img => img.id)
+
+      const startIndex = newImgIds.indexOf(lastImgId)
+      if (startIndex === -1) {
+        state.images.push(...newImgs)
+      } else {
+        state.images.push(...newImgs.slice(startIndex + 1, newImgs.length - 1))
+      }
+    }
   }
 }
 
