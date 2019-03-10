9c78ef5cebe1c3efd4ec80c210327a527919eb28
dora1998 <dorayaki.app@gmail.com>
2019-03-10 10:52:32 +0900

    Polyfill効いてなかった問題を修正

--- nuxt.config.js
+++ nuxt.config.js
@@ -38,7 +38,7 @@ module.exports = {
   /*
   ** Plugins to load before mounting the App
   */
-  plugins: ['@/plugins/vuetify', '@/plugins/api'],
+  plugins: ['@/plugins/vuetify', '@/plugins/api', '@/plugins/polyfill'],
 
   /*
   ** Nuxt.js modules
